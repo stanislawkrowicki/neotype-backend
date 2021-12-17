6179c40ce583a2b16df4ee609e0d9032afa4c7a2
Stanislaw Krowicki <krowicki00@gmail.com>
2021-12-17 19:09:49 +0100

    Add Content-Type to CORS allowed headers

--- cmd/web-api/main.go
+++ cmd/web-api/main.go
@@ -14,7 +14,7 @@ func main() {
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000"},
 		AllowMethods:     []string{"GET", "POST", "UPDATE"},
-		AllowHeaders:     []string{"Origin"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 	}))
@@ -22,6 +22,10 @@ func main() {
 	router.GET("/words/:count", func(c *gin.Context) {
 		gateway.Proxy(c, "words", fmt.Sprintf("/words/%s", c.Param("count")))
 	})
+	router.POST("/login", func(c *gin.Context) {
+		gateway.Proxy(c, "users", "/login")
+	})
+
 	port, err := config.Get("web-api", "port")
 	if err != nil {
 		panic(err)
