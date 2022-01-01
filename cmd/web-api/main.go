ab14cbb95fbbe01d07b9d53262de869206054670
Stanislaw Krowicki <krowicki00@gmail.com>
2022-01-01 15:56:15 +0100

    Add production url to CORS

--- cmd/web-api/main.go
+++ cmd/web-api/main.go
@@ -12,7 +12,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000", "https://neotype.herokuapp.com"},
 		AllowMethods:     []string{"GET", "POST", "UPDATE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
