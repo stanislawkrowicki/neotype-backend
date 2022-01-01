d7729515233abbf5fa3b4e479c1bd8175c44e990
Stanislaw Krowicki <krowicki00@gmail.com>
2022-01-01 15:09:30 +0100

    config for prod

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+SERVICES="web-api words"
+
+echo "Deploying ${SERVICES}"
+
+for SERVICE in ${SERVICES}; do
+  heroku buildpacks:add -a "${SERVICE}-neotype" heroku-community/multi-procfile
+  heroku config:set -a "${SERVICE}-neotype" PROCFILE=/cmd/"${SERVICE}"/Procfile
+  git push https://git.heroku.com/"${SERVICE}-neotype".git HEAD:master
+done
