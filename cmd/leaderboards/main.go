f1a60a60f24c9f2f2d82cfeb9b469cc8965122a2
Stanislaw Krowicki <krowicki00@gmail.com>
2022-01-01 16:29:57 +0100

    Revert adding CORS

--- cmd/leaderboards/main.go
+++ cmd/leaderboards/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"neotype-backend/pkg/config"
 	"neotype-backend/pkg/leaderboards"
@@ -10,14 +9,6 @@ import (
 
 func main() {
 	router := gin.Default()
-
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000", "https://neotype.herokuapp.com"},
-		AllowMethods:     []string{"GET", "POST"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-	}))
 
 	router.POST("/leaderboards", leaderboards.Entry)
 	router.GET("/leaderboards/:count", leaderboards.Leaders)
