ded1aba3aaea89fe60f714c7b616e7a3dd8141d5
Stanislaw Krowicki <krowicki00@gmail.com>
2022-01-01 15:28:33 +0100

    add go buildpack

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ echo "Deploying ${SERVICES}"
 
 for SERVICE in ${SERVICES}; do
   heroku buildpacks:add -a "${SERVICE}-neotype" heroku-community/multi-procfile
+  heroku buildpacks:add -a "${SERVICE}-neotype" heroku/go
   heroku config:set -a "${SERVICE}-neotype" PROCFILE=/cmd/"${SERVICE}"/Procfile
   git push https://git.heroku.com/"${SERVICE}-neotype".git HEAD:master
 done
